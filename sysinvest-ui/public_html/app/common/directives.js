97b142c6561fec29b186b480008bb3be2f116438
salirajr <jovirsalira@gmail.com>
2016-08-24 20:32:42 +0700

    Add iCheck for Angular, Investor form.

--- sysinvest-ui/public_html/app/common/directives.js
+++ sysinvest-ui/public_html/app/common/directives.js
@@ -10,7 +10,8 @@
             .directive('sideNavigation', sideNavigation)
             .directive('iboxTools', iboxTools)
             .directive('minimalizaSidebar', minimalizaSidebar)
-            .directive('iboxToolsFullScreen', iboxToolsFullScreen);
+            .directive('iboxToolsFullScreen', iboxToolsFullScreen)
+            .directive('icheck', iCheckRender);
 
 
     /*
@@ -155,6 +156,46 @@
                 };
             }
         };
+    }
+
+    function iCheckRender($timeout, $parse) {
+        return {
+            restrict: 'A',
+            require: '?ngModel',
+            link: function (scope, element, attr, ngModel) {
+                $timeout(function () {
+                    var value = attr.value;
+
+                    function update(checked) {
+                        if (attr.type === 'radio') {
+                            ngModel.$setViewValue(value);
+                        } else {
+                            ngModel.$setViewValue(checked);
+                        }
+                    }
+
+                    $(element).iCheck({
+                        checkboxClass: attr.checkboxClass || 'icheckbox_square-green',
+                        radioClass: attr.radioClass || 'iradio_square-green'
+                    }).on('ifChanged', function (e) {
+                        scope.$apply(function () {
+                            update(e.target.checked);
+                        });
+                    });
+
+                    scope.$watch(attr.ngChecked, function (checked) {
+                        if (typeof checked === 'undefined')
+                            checked = !!ngModel.$viewValue;
+                        update(checked)
+                    }, true);
+
+                    scope.$watch(attr.ngModel, function (model) {
+                        $(element).iCheck('update');
+                    }, true);
+
+                })
+            }
+        }
     }
 
 })();
